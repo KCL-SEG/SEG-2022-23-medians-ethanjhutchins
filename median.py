5219be965e7a8b56d51eae5dc1f8b6283b86bb3a
Ethan Hutchins <ethanjameshutchins@gmail.com>
2022-10-02 19:05:21 +0100

    Add median functionality.

--- median.py
+++ median.py
@@ -5,8 +5,13 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+        if (len(numbers)%2==1):
+        	median = numbers[len(numbers)//2]
+        else:
+            median = (numbers[len(numbers)//2] + numbers[(len(numbers)//2)-1])/2
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+print(median)
